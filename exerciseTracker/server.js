bbb8b9eb5b077c0a401d732a537c043c2d25b1e7
Nate Andrews <35821125+natea123@users.noreply.github.com>
2021-08-15 10:31:43 -0400

    updated routes

--- exerciseTracker/server.js
+++ exerciseTracker/server.js
@@ -8,6 +8,8 @@ app.use(cors())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}));
 
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
@@ -18,9 +20,9 @@ app.post('/api/users', (req, res) => {
   
   var id = RandomHexString(8);
   users.push({
-    "username": req.body.username,
-    "_id": id,
-    "exercise_log": {},
+    _id: id,
+    username: req.body.username,
+    log: [],
   })
   res.json({ username : req.body.username, _id: id })
 
@@ -31,6 +33,42 @@ app.get('/api/users', (req, res) => {
   res.json(result)
 });
 
+app.post('/api/users/:id/exercises', (req, res) => {
+
+  var input_date = req.body.date;
+
+  //checks for empty date string and sets as today if empty
+  if (input_date == '') {
+    today = new Date();
+    var input_date = today.toDateString();
+  } else {
+    var input_date = new Date(req.body.date);
+    input_date = input_date.toDateString();
+  }
+  
+
+  //finds object in users array corresponding to given _id 
+  var input_user = users.find( ({ _id }) => _id === req.params.id );
+
+  //push exercise log to master user profile
+  input_user.log.push({
+    date: input_date,
+    duration: parseInt(req.body.duration),
+    description: req.body.description,
+  });
+
+  //created temp user object to respond back to user confirming exercise was logged
+  var response_object = new Object();
+  response_object._id = input_user._id;
+  response_object.username = input_user.username;
+  response_object.date = input_date;
+  response_object.duration = parseInt(req.body.duration);
+  response_object.description = req.body.description;
+
+
+  return res.json(response_object);
+
+});
 
 function RandomHexString(L) {
   var hexstring='';
